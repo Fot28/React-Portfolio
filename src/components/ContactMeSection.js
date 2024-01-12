ae212f8d72c887e1acf87fd418cfaed6e3be52e1
Fot28 <126478625+Fot28@users.noreply.github.com>
2024-01-12 21:06:02 +0000

    completed the form with validation and alerts

--- src/components/ContactMeSection.js
+++ src/components/ContactMeSection.js
@@ -1,91 +1,141 @@
-import React, {useEffect} from "react";
-import { useFormik } from "formik";
+import React, { useEffect } from "react"
+import { useFormik } from "formik"
 import {
-  Box,
-  Button,
-  FormControl,
-  FormErrorMessage,
-  FormLabel,
-  Heading,
-  Input,
-  Select,
-  Textarea,
-  VStack,
-} from "@chakra-ui/react";
-import * as Yup from 'yup';
-import FullScreenSection from "./FullScreenSection";
-import useSubmit from "../hooks/useSubmit";
-import {useAlertContext} from "../context/alertContext";
+	Box,
+	Button,
+	FormControl,
+	FormErrorMessage,
+	FormLabel,
+	Heading,
+	Input,
+	Select,
+	Textarea,
+	VStack,
+	useToast,
+} from "@chakra-ui/react"
+import * as Yup from "yup"
+import FullScreenSection from "./FullScreenSection"
+import useSubmit from "../hooks/useSubmit"
 
-const LandingSection = () => {
-  const {isLoading, response, submit} = useSubmit();
-  const { onOpen } = useAlertContext();
+const ContactMeSection = () => {
+	const toast = useToast()
+	const { isLoading, response, submit } = useSubmit()
 
-  const formik = useFormik({
-    initialValues: {},
-    onSubmit: (values) => {},
-    validationSchema: Yup.object({}),
-  });
+	const formik = useFormik({
+		initialValues: {
+			firstName: "",
+			email: "",
+			type: "", // default value can be 'hireMe', 'openSource', or 'other' if needed
+			comment: "",
+		},
+		onSubmit: (values, { resetForm }) => {
+			submit("/api/submitForm", values).then(() => {
+				// check for success response and reset form
+				if (response?.type === "success") {
+					resetForm()
+				}
+			})
+		},
+		validationSchema: Yup.object({
+			firstName: Yup.string().required("Required"),
+			email: Yup.string().email("Invalid email address").required("Required"),
+			type: Yup.string(), // No validation rules needed as it's optional
+			comment: Yup.string()
+				.min(25, "Must be at least 25 characters")
+				.required("Required"),
+		}),
+	})
 
-  return (
-    <FullScreenSection
-      isDarkBackground
-      backgroundColor="#512DA8"
-      py={16}
-      spacing={8}
-    >
-      <VStack w="1024px" p={32} alignItems="flex-start">
-        <Heading as="h1" id="contactme-section">
-          Contact me
-        </Heading>
-        <Box p={6} rounded="md" w="100%">
-          <form>
-            <VStack spacing={4}>
-              <FormControl isInvalid={false}>
-                <FormLabel htmlFor="firstName">Name</FormLabel>
-                <Input
-                  id="firstName"
-                  name="firstName"
-                />
-                <FormErrorMessage></FormErrorMessage>
-              </FormControl>
-              <FormControl isInvalid={false}>
-                <FormLabel htmlFor="email">Email Address</FormLabel>
-                <Input
-                  id="email"
-                  name="email"
-                  type="email"
-                />
-                <FormErrorMessage></FormErrorMessage>
-              </FormControl>
-              <FormControl>
-                <FormLabel htmlFor="type">Type of enquiry</FormLabel>
-                <Select id="type" name="type">
-                  <option value="hireMe">Freelance project proposal</option>
-                  <option value="openSource">
-                    Open source consultancy session
-                  </option>
-                  <option value="other">Other</option>
-                </Select>
-              </FormControl>
-              <FormControl isInvalid={false}>
-                <FormLabel htmlFor="comment">Your message</FormLabel>
-                <Textarea
-                  id="comment"
-                  name="comment"
-                  height={250}
-                />
-                <FormErrorMessage></FormErrorMessage>
-              </FormControl>
-              <Button type="submit" colorScheme="purple" width="full">
-                Submit
-              </Button>
-            </VStack>
-          </form>
-        </Box>
-      </VStack>
-    </FullScreenSection>
-  );
-};
+	useEffect(() => {
+		// if there's a response and it's not loading, trigger the toast
+		if (response && !isLoading) {
+			toast({
+				title: response.type === "success" ? "All good!" : "Oops!",
+				description: response.message,
+				status: response.type,
+				duration: 5000,
+				isClosable: true,
+			})
+		}
+	}, [response, isLoading, toast])
 
-export default LandingSection;
+	return (
+		<FullScreenSection
+			isDarkBackground
+			backgroundColor="#512DA8"
+			py={16}
+			spacing={8}
+		>
+			<VStack w="1024px" p={32} alignItems="flex-start">
+				<Heading as="h1" id="contactme-section">
+					Contact me
+				</Heading>
+				<Box p={6} rounded="md" w="100%">
+					<form onSubmit={formik.handleSubmit}>
+						<VStack spacing={4}>
+							<FormControl
+								isInvalid={formik.touched.firstName && formik.errors.firstName}
+							>
+								<FormLabel htmlFor="firstName">Name</FormLabel>
+								<Input
+									id="firstName"
+									name="firstName"
+									{...formik.getFieldProps("firstName")}
+								/>
+								<FormErrorMessage>{formik.errors.firstName}</FormErrorMessage>
+							</FormControl>
+							<FormControl
+								isInvalid={formik.touched.email && formik.errors.email}
+							>
+								<FormLabel htmlFor="email">Email Address</FormLabel>
+								<Input
+									id="email"
+									name="email"
+									type="email"
+									{...formik.getFieldProps("email")}
+								/>
+								<FormErrorMessage>{formik.errors.email}</FormErrorMessage>
+							</FormControl>
+							<FormControl>
+								<FormLabel htmlFor="type">Type of enquiry</FormLabel>
+								<Select id="type" name="type" {...formik.getFieldProps("type")}>
+									<option value="hireMe" style={{ color: "#512DA8" }}>
+										Freelance project proposal
+									</option>
+									<option value="openSource" style={{ color: "#512DA8" }}>
+										Open source consultancy session
+									</option>
+									<option value="other" style={{ color: "#512DA8" }}>
+										Other
+									</option>
+								</Select>
+							</FormControl>
+
+							<FormControl
+								isInvalid={formik.touched.comment && formik.errors.comment}
+							>
+								<FormLabel htmlFor="comment">Your message</FormLabel>
+								<Textarea
+									id="comment"
+									name="comment"
+									{...formik.getFieldProps("comment")}
+								/>
+								<FormErrorMessage>{formik.errors.comment}</FormErrorMessage>
+							</FormControl>
+							<Button
+								type="submit"
+								colorScheme="purple"
+								width="full"
+								isLoading={isLoading}
+							>
+								Submit
+							</Button>
+						</VStack>
+					</form>
+				</Box>
+			</VStack>
+		</FullScreenSection>
+	)
+}
+
+export default ContactMeSection
